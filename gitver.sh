f64a0811fe504d02c90217df9d3519252a6a7aa9
tm512 <elykdav@gmail.com>
2014-06-06 22:49:16 -0700

    oops

--- gitver.sh
+++ gitver.sh
@@ -4,10 +4,8 @@ if [ -d .git ] ; then
 	out="$(git rev-parse --short HEAD)"
 
 	if [ $(uname | grep -i bsd) ] ; then
-		echo "bsd"
 		out="${out}-$(date -j -f "%+" "$(git log -n1 $out | grep "^Date" | sed 's/Date:   //;s/-[0-9]*//')" +"%Y%m%d")"
 	else
-		echo "linux"
 		out="${out}-$(date -d"$(git log -n1 $out | grep "^Date" | sed 's/Date:   //;s/-[0-9]*//')" +"%Y%m%d")"
 	fi
 
